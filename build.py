c7c11a28bfe0aae748b26b4efe1c3a5d30b85c69
sagard21 <sagard21@users.noreply.github.com>
2017-08-09 19:01:32 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,11 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    dic1 = {}
+    e_cnt = 0
+    o_cnt = 0
+    for i in list_of_nums:
+        if i % 2 == 0:
+            e_cnt += 1
+        else:
+            o_cnt += 1
+    dic1 = {'ODD': o_cnt, 'EVEN': e_cnt}
+    return dic1
